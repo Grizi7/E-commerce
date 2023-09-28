28f8157c5b2a337b5717fcd10024088495978be0
Eman Zaki <88564710+emanzaki@users.noreply.github.com>
2023-09-28 21:51:08 +0300

    dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,18 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Zidan',
+            'email' => 'zidan@zidan.com',
+            'password' => bcrypt('zidan123'),
+            'role' => true,
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Eman',
+            'email' => 'eman@eman.com',
+            'password' => bcrypt('eman123'),
+            'role' => true,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/admin', function () {
+    return view('admin.auth.signin');
+});
+
+
+Route::group(['middleware'=>[]],function(){
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    });
 });
